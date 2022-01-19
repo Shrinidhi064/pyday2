7d6a1cb9cf2d8f4a13fa4acb4d4721cccfed1993
Shrinidhi064 <97896389+Shrinidhi064@users.noreply.github.com>
2022-01-19 16:50:00 +0530

    Submission done!!

---
+++ pyassignment2.py
@@ -0,0 +1,23 @@
+a=7
+b=5.444
+boolean_1=True
+boolean_2=False
+tuple_1=("Python","C++","C")
+list_1=["Computer Sci","Biology","Arts"]
+set_1={1,2,4,67,7}
+dict_1={"Name":"Shrinidhi","Class":"12","Place":"Gujarat"}
+print(a)
+print(b)
+print(boolean_1)
+print(boolean_2)
+print(max(set_1))
+print(sum(set_1))
+print(tuple_1)
+print(list_1)
+print(set_1)
+print(dict_1)
+for i in range(0,len(list_1)):
+    print(list_1[i])
+print(f"My fav subject is {tuple_1[0]}")
+tuple_1=list(tuple_1)
+print(tuple_1)
